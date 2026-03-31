0261b5e0f5a0b0984b5744dbfb38be1c48094775
oktavianuu <oktavianuss.misro@gmail.com>
2026-03-31 10:45:39 +0700

    later I'll continue

--- essentials/functions/leap_year.py
+++ essentials/functions/leap_year.py
@@ -7,7 +7,28 @@ def is_year_leap(year):
         return True
     else:
         return False
-        
+
+    # if year % 4 != 0:
+    #     return False
+    # elif year % 100 != 0:
+    #     return True
+    # elif year % 400 != 0:
+    #     return False
+    # else:
+    #     return True
+
+def days_in_month(year, month):
+    # if statement
+
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # number id days month 1-12
+    res = days[month-1]
+    if month == 2 and is_year_leap(year):
+        res = 29
+    return res
+
+
+def day_of_year(year, month, day):
+    None
 
 test_data = [1900, 2000, 2016, 1987]
 test_results = [False, True, True, False]
